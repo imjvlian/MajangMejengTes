b6b4090765759e04d78c1644eddbb67eee470fef
jvlian <julianvikbusiness@gmail.com>
2026-08-14 16:42:55 +0700

    1.0.1

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import categoryRoutes from "./routes/category.route.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -24,9 +25,23 @@ const app = express();
 // for allowing json object in req body
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "https://DOMAIN-FRONTEND-KAMU.vercel.app",
+    credentials: true,
+  })
+);
 
-app.listen(5000, () => {
-  console.log("Server is running on port 5000!");
+app.get("/", (req, res) => {
+  res.json({
+    message: "Morning Dispatch API is running!",
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
 
 app.use("/api/auth", authRoutes);
